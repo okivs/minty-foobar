025432aa43cfb11f3029d0e582b0237cd22b82a9
okivs <saaaaiko@gmail.com>
2022-09-19 04:45:31 -0700

    feature: add context menu to playlist tracker

--- user-components/foo_spider_monkey_panel/custom-scripts/playlist-display.js
+++ user-components/foo_spider_monkey_panel/custom-scripts/playlist-display.js
@@ -1,21 +1,113 @@
 ﻿include(fb.ComponentPath + 'samples\\complete\\js\\lodash.min.js');
 include(fb.ComponentPath + 'samples\\complete\\js\\helpers.js');
-include(fb.ComponentPath + 'samples\\complete\\js\\panel.js');
 
-colorFont = window.GetColourCUI(1);
-colorBG = window.GetColourCUI(4);
-var font = gdi.Font('Quicksand Medium',14,0);
-var panel = new _panel();
+let colorFont = window.GetColourCUI(1);
+let colorBG = window.GetColourCUI(4);
+let font = gdi.Font('Quicksand Medium',14,0);
 
-var len;
-var size;
-var count;
+let len;
+let size;
+let count;
 
-var viewingPlaylist;
-var playingPlaylist;
-var playStatus;
+let viewingPlaylist;
+let playingPlaylist;
+let playStatus;
 
-// ---- on playlist state
+function makeContextMenu(x, y) {
+    let contextManager = fb.CreateContextMenuManager();
+    let baseMenu = window.CreatePopupMenu();
+    let plMenu = window.CreatePopupMenu();
+    let plStartAt = 7; // id that playlists start at, to ensure they should never overlaps with menu ids
+    
+    /* context menu indexes 1-based because 0 refers to clicking out of the menu,
+       playlist list is 0-indexed */
+    baseMenu.AppendMenuItem(MF_STRING, 1, "Configure...");
+    baseMenu.AppendMenuItem(MF_STRING, 2, "Edit panel script...");
+    baseMenu.AppendMenuSeparator();
+    
+    plMenu.AppendMenuItem(MF_STRING, 3, "Create new playlist");
+    plMenu.AppendMenuItem(MF_STRING, 4, "Load playlist from file");
+    plMenu.AppendMenuItem(MF_STRING, 5, "Rename current playlist");
+    plMenu.AppendMenuSeparator();
+    plMenu.AppendMenuItem(MF_STRING, 6, "Remove current playlist");
+    plMenu.AppendTo(baseMenu, MF_STRING, "Playlist...")
+    
+    baseMenu.AppendMenuSeparator();
+    
+    if (plman.PlaylistCount > 0) {
+        for (let i = 0; i < plman.PlaylistCount; i++) {
+            let listName = plman.GetPlaylistName(i);
+            let listCount = plman.PlaylistItemCount(i);
+            i === plman.ActivePlaylist ? baseMenu.AppendMenuItem(0x00000008, i + plStartAt, `${listName} \t(${listCount})`)
+                                       : baseMenu.AppendMenuItem(MF_STRING, i + plStartAt, `${listName} \t(${listCount})`);
+        }
+    }
+    
+    const idx = baseMenu.TrackPopupMenu(x, y);
+    let newName = "";
+    switch (true) {
+        case (idx <= 0):
+            return;
+        case (idx === 1):
+            window.ShowConfigureV2()
+            break;
+        case (idx === 2):
+            window.EditScript() 
+            break;
+        case (idx === 3):
+            try {
+                newName = utils.InputBox(0, "Enter a name for the new playlist:", "Create new playlist", "", true);
+            
+                // Create new playlist at end of playlist list and then go to it.
+                plman.CreatePlaylist(plman.PlaylistCount + 1, newName);
+                plman.ActivePlaylist = plman.PlaylistCount - 1;
+            } catch { return; }
+            break;
+        case (idx === 4):
+            fb.LoadPlaylist()
+            break;
+        case (idx === 5):
+            try {
+                newName = utils.InputBox(0, 
+                                         `Enter a new name for ${plman.GetPlaylistName(plman.ActivePlaylist)}:`, 
+                                         "Rename current playlist", "", true);
+                plman.RenamePlaylist(plman.ActivePlaylist, newName)
+                getPlaylistInfo();
+            } catch { return; }
+            break;
+        case (idx === 6):
+            plman.RemovePlaylistSwitch(plman.ActivePlaylist);
+            break;
+        case (idx > 6):
+            plman.ActivePlaylist = idx - plStartAt;
+            break;
+        default:
+            return;
+    }
+}
+
+function checkViewState() {
+    if (fb.GetNowPlaying() == null) {
+
+        return `${plman.GetPlaylistName(activePlaylist)}`;
+
+    } else if (playingPlaylist === viewingPlaylist && fb.IsPaused) {
+
+        return `▶️ ${playingPlaylist}`;
+
+    } else if (playingPlaylist === viewingPlaylist && fb.IsPlaying) {
+
+        return `▶️ ${playingPlaylist}`;
+
+    } else if (playingPlaylist !== viewingPlaylist) {
+
+        let state = fb.IsPaused ? `▶️ ${playingPlaylist}` : `▶️ ${playingPlaylist}`;
+        return `${state} • ${plman.GetPlaylistName(activePlaylist)}`;
+
+    } else {
+        return;
+    }
+}
 
 function getPlaylistInfo() {
     activePlaylist = plman.ActivePlaylist;
@@ -26,7 +118,9 @@ function getPlaylistInfo() {
     playStatus = checkViewState();
     window.Repaint();
 }
-getPlaylistInfo(); 
+getPlaylistInfo();
+
+// ---- on playlist state
 
 function on_playlist_switch() {
     getPlaylistInfo();
@@ -41,8 +135,8 @@ function on_playlist_items_removed(playlist, new_count) {
 }
 
 function on_colours_changed() {
-    colorFont = window.GetColourCUI(5);
-    barColorBG = window.GetColourCUI(4);
+    colorFont = window.GetColourCUI(1);
+    colorBG = window.GetColourCUI(4);
     window.Repaint();
 }
 
@@ -73,35 +167,20 @@ function on_playback_stop() {
     window.Repaint();
 }
 
-function checkViewState() {
-    if (fb.GetNowPlaying() == null) {
-
-        return `${plman.GetPlaylistName(activePlaylist)}`;
-
-    } else if (playingPlaylist === viewingPlaylist && fb.IsPaused) {
-
-        return `▶️ ${playingPlaylist}`;
-
-    } else if (playingPlaylist === viewingPlaylist && fb.IsPlaying) {
-
-        return `▶️ ${playingPlaylist}`;
-
-    } else if (playingPlaylist !== viewingPlaylist) {
-
-        let state = fb.IsPaused ? `▶️ ${playingPlaylist}` : `▶️ ${playingPlaylist}`;
-        return `${state} • ${plman.GetPlaylistName(activePlaylist)}`;
-
-    } else {
-        return;
-    }
-}
-
 // ----
 
 function on_size() {
-	panel.size();
-	w = panel.w;
-	h = panel.h;
+	w = window.Width;
+	h = window.Height;
+}
+
+function on_mouse_lbtn_up(x, y) {
+    makeContextMenu(x, y);
+}
+
+function on_mouse_rbtn_up(x, y) {
+    makeContextMenu(x, y);
+    return true;
 }
 
 function on_paint(gr) {
@@ -113,6 +192,7 @@ function on_paint(gr) {
         infoText = `${plman.GetPlaylistName(activePlaylist)} (${playlistItemCount})`;
     }
     
-    gr.FillSolidRect(0 , 0, w, h, colorBG);
+    console.log(w)
+    gr.FillSolidRect(0, 0, w, h, colorBG);
     gr.GdiDrawText(infoText, font, colorFont, 6, 0, w - 12, h - 2, 0x00000020|DT_VCENTER|DT_CENTER|0x00040000);
 }
